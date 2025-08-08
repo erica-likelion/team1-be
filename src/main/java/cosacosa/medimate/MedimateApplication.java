13322dd1cf9fa80ed490cd3d1c5f21222be7f1ef
mongsam2 <songjaehyun121476@gmail.com>
2025-08-08 21:07:29 +0900

    init

---
+++ src/main/java/cosacosa/medimate/MedimateApplication.java
@@ -0,0 +1,13 @@
+package cosacosa.medimate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedimateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedimateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cosacosa/medimate/MedimateApplicationTests.java
@@ -0,0 +1,13 @@
+package cosacosa.medimate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedimateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
