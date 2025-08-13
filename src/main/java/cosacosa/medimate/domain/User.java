84509d8ca9a0b45bd5c8d9de6f281fdf8b870af5
mongsam2 <songjaehyun121476@gmail.com>
2025-08-13 18:45:49 +0900

    User 엔티티 작성

---
+++ src/main/java/cosacosa/medimate/domain/User.java
@@ -0,0 +1,23 @@
+package cosacosa.medimate.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String prescriptionHistory = "";
+}
