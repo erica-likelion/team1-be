f5e2ca63218fa2b48c3ff0bfd74cd9877dc8a1e5
mongsam2 <songjaehyun121476@gmail.com>
2025-08-13 18:59:57 +0900

    Prescription에 한국어로된 처방전 설명 AI 생성글을 저장할 필드 (koreanContent) 추가

--- src/main/java/cosacosa/medimate/domain/Prescription.java
+++ src/main/java/cosacosa/medimate/domain/Prescription.java
@@ -12,9 +12,11 @@ public class Prescription {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String caution; // 처방전 정보를 바탕으로 AI가 생성한 설명글
+    private String content; // AI가 생성한 처방전 설명 글 (사용자 설정 언어)
+
+    private String koreanContent; // AI가 생성한 처방전 설명 글 (한국어)
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(nullable = false)
-    private Long userId; // 처방전을 입력한 사용자
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user; // 처방전을 입력한 사용자
 }
