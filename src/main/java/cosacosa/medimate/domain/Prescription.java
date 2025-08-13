744088f68985829c4c5b8f356b5e844954319de7
mongsam2 <songjaehyun121476@gmail.com>
2025-08-13 20:53:35 +0900

    Prescription: createdAt 속성 추가 및 기본 값으로 객체가 생성된 날짜로 저장

--- src/main/java/cosacosa/medimate/domain/Prescription.java
+++ src/main/java/cosacosa/medimate/domain/Prescription.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Entity
 @NoArgsConstructor
 @Getter
@@ -12,11 +14,18 @@ public class Prescription {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String content; // AI가 생성한 처방전 설명 글 (사용자 설정 언어)
+    private String content = ""; // AI가 생성한 처방전 설명 글 (사용자 설정 언어)
 
-    private String koreanContent; // AI가 생성한 처방전 설명 글 (한국어)
+    private String koreanContent = ""; // AI가 생성한 처방전 설명 글 (한국어)
+
+    private LocalDate createdAt;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(nullable = false, name = "user_id")
     private User user; // 처방전을 입력한 사용자
+
+    @PrePersist
+    void onCreate() {
+        this.createdAt = LocalDate.now();
+    }
 }
