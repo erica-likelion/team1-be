8729ddfc23b5cc439b9e858e6caa4e010db945aa
mongsam2 <songjaehyun121476@gmail.com>
2025-08-13 20:59:08 +0900

    Prescription: GET 응답용 dto 작성 (상세 조회)

--- src/main/java/cosacosa/medimate/dto/PrescriptionResponse.java
+++ src/main/java/cosacosa/medimate/dto/PrescriptionResponse.java
@@ -1,4 +1,15 @@
 package cosacosa.medimate.dto;
 
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
 public class PrescriptionResponse {
+    private Long id;
+    private String title;
+    private String content;
+    private String koreanContent;
+    private LocalDate createdAt;
+    private String image; // 의약품 공공 api에서 가져온 이미지 파일의 url
 }
