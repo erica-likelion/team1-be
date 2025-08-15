5dc8e90879a394d222954026d66692523d95eeb6
mongsam2 <songjaehyun121476@gmail.com>
2025-08-15 14:30:40 +0900

    Prescription: dto에 생성자 추가

--- src/main/java/cosacosa/medimate/dto/PrescriptionListItemResponse.java
+++ src/main/java/cosacosa/medimate/dto/PrescriptionListItemResponse.java
@@ -9,4 +9,10 @@ public class PrescriptionListItemResponse {
     private Long id;
     private String title;
     private LocalDate createdAt;
+
+    public PrescriptionListItemResponse(Long id, String title, LocalDate createdAt) {
+        this.id = id;
+        this.title = title;
+        this.createdAt = createdAt;
+    }
 }
--- src/main/java/cosacosa/medimate/dto/PrescriptionResponse.java
+++ src/main/java/cosacosa/medimate/dto/PrescriptionResponse.java
@@ -11,5 +11,14 @@ public class PrescriptionResponse {
     private String content;
     private String koreanContent;
     private LocalDate createdAt;
-    private String image; // 의약품 공공 api에서 가져온 이미지 파일의 url
+    private String image = ""; // 의약품 공공 api에서 가져온 이미지 파일의 url
+
+    public PrescriptionResponse(Long id, String title, String content, String koreanContent, LocalDate createdAt, String image) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.koreanContent = koreanContent;
+        this.createdAt = createdAt;
+        this.image = image;
+    }
 }
